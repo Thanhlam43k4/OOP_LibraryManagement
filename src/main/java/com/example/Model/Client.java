7f7c455434647a3a126891ae9b6b65423364ec27
Thanhlam43k4 <lamcoivodoi123@gmail.com>
2024-10-27 19:14:22 +0700

    Update sql.init and delete target folder

--- src/main/java/com/example/Model/Client.java
+++ src/main/java/com/example/Model/Client.java
@@ -1,7 +1,14 @@
 package com.example.Model;
 
 public class Client extends User {
+    private int borrowed_book;
+
+
     public Client(String email, String password) {
         super(email, password);
+    }
+    public Client(String email,String password, int borrowed_book){
+        super(email,password);
+        this.borrowed_book = borrowed_book;
     }
 }
