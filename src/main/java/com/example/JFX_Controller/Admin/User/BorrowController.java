1a19848c2fd7a09434cc9e4cd68be7c02362b1ff
tronganhng <latgoto2004@gmail.com>
2024-11-18 16:49:44 +0700

    admin borrow doc

--- src/main/java/com/example/JFX_Controller/Admin/User/BorrowController.java
+++ src/main/java/com/example/JFX_Controller/Admin/User/BorrowController.java
@@ -1,15 +1,22 @@
 package com.example.JFX_Controller.Admin.User;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+
+import com.example.Handlers.ExtraFunction;
+import com.example.Handlers.Notify;
+import com.example.Handlers.Validate;
+import com.example.Service.TransactionService;
+
 import javafx.event.ActionEvent;
 
 public class BorrowController {
 
     @FXML private TextField docId;
-    @FXML private TextField isbn;
+    @FXML private TextField isbn; // của copy
     
     @FXML private StackPane userBorrowRoot;
     @FXML private AnchorPane userPane;
@@ -17,6 +24,15 @@ public class BorrowController {
     private int userId;
     @FXML
     void borrowDoc(ActionEvent event) {
+        String isbn_input = isbn.getText();
+        isbn_input = ExtraFunction.extractISBN(isbn_input);
+        if (!Validate.isValidISBN(isbn_input)) {
+            Notify.showAlert(Alert.AlertType.ERROR, "Eror", "ISBN invalid!");
+            return;
+        }
+
+        TransactionService.instance.borrowBook(userId, isbn_input);
+        Notify.showAlert(Alert.AlertType.INFORMATION, "Nofication", "Borrow Document sucess!");
         userPane.getChildren().remove(this.userBorrowRoot);
         this.userBorrowRoot = null;
     }
