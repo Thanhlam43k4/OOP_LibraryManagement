bf0f2d4e195d772da8dab2a81e0fe9c592e5d409
tronganhng <latgoto2004@gmail.com>
2024-11-22 11:08:49 +0700

    fixui

--- src/main/java/com/example/JFX_Controller/Client/DocInfoController.java
+++ src/main/java/com/example/JFX_Controller/Client/DocInfoController.java
@@ -75,6 +75,7 @@ public class DocInfoController extends Controller implements Initializable{
         this.userName.setText(SessionManager.getInstance().getLoggedInUser().getUsername());
         this.title.setText(doc.getTitle().toUpperCase());
         this.author.setText(doc.getAuthor());
+        description.setText(doc.getDescription());
         // query description here
         // check borrow
         setBorrowBut();
