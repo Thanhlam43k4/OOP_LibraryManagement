3a20ef30735fca4660b57e93ba0f0d3ae79ff9ee
tronganhng <latgoto2004@gmail.com>
2024-11-28 00:52:25 +0700

    addUser node

--- src/main/java/com/example/JFX_Controller/Admin/User/AddUserController.java
+++ src/main/java/com/example/JFX_Controller/Admin/User/AddUserController.java
@@ -82,7 +82,7 @@ public class AddUserController {
             String password = ExtraFunction.encode("1");
             User user = new User(email_input, userName_input,password, phone_input, age_input);
             UserService.instance.addUser(user);
-            //loadUserNode(UserService.instance.getUserByEmail(email_input));
+            loadUserNode(UserService.instance.getClientByEmail(email_input));
         }
 
         // Remove the Add User pane from the UI after a successful operation
